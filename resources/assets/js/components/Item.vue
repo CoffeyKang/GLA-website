396f3e44cbf9a1422b518974f74c265e034d3c31
coffeykang <coffeykang@gmail.com>
2018-01-02 16:28:26 -0500

    Home page brief layout finished

---
+++ resources/assets/js/components/Item.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+		<div class="img">	
+			<img src="/images/img.jpg" alt="img">
+		</div>
+		<div class="words">
+			
+			<small>Product No: #123456</small>
+			
+			<span class='description'>Description</span>
+			
+			<small>Year Fit: 1990-1980</small>
+			
+			<small>Make: WHEEL</small>
+			
+			<span class='price'>$200.00</span>
+			
+			<button class="btn btn-primary">
+				Details
+			</button>
+			
+
+		</div>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style scoped>
+	div{
+		min-height: 100px;
+	}
+	img{
+		width:90%;
+	}
+	.words{
+		display: flex;
+		flex-direction: column;
+	}
+	button{
+		width:150px;
+	}
+	.price{
+		color: red;
+		font-size: 2em;
+		font-weight: bold;
+		padding: 10px 0 ;
+	}
+	.description{
+		color: black;
+		font-weight: bold;
+		text-transform: uppercase;
+	}
+</style>
--- resources/assets/js/components/body/Home.vue
+++ resources/assets/js/components/body/Home.vue
@@ -6,36 +6,95 @@
                   <h3>{{ item }}</h3>
                 </el-carousel-item>
             </el-carousel>
-            
-            <div class="searchBar">
+        </div>
+
+        <div class="searchBar">
                 <form class="form-inline">
                   <div class="form-group">
                     <label class='form-label'>I NEED TO FIND: </label>
                     <input type="text" class="form-control" placeholder="Enter a specific part, ie. Fender, Hood, Lamp, etc.">
                   </div>
-                  <div class="form-group mx-sm-3">
+                  <div class="form-group">
                     <label for="inputPassword2" class="sr-only">Make</label>
                     <input type="password" class="form-control" placeholder="Make">
                   </div>
-                  <div class="form-group mx-sm-3">
+                  <div class="form-group ">
                     <label for="inputPassword2" class="sr-only">Year</label>
                     <input type="password" class="form-control" placeholder="Year">
                   </div>
                   <button type="submit" class="btn btn-primary"> FIND </button>
                 </form>
-            </div>
-           
-                
-                
         </div>
+
+        <div class="advertisements">
+        
+          <div class="ad">
+            ad1
+          </div>
+          <div class="ad">
+            ad2
+          </div>
+          <div class="ad">
+            ad3
+          </div>          
+
+        </div>
+
+        <div class="feature">
+          <div class="sub_title">
+            <span>FEATURE PRODUCTS</span>
+          </div>
+          <div class='feature_item'>
+            <div class='first_item'>
+              <app-item></app-item>
+            </div>
+            <div>
+              <app-item></app-item>
+            </div>
+            <div>
+              <app-item></app-item>
+            </div>
+            <div class='right_item'>
+              <app-item></app-item>
+            </div>
+          </div>
+        </div>
+
+        <div class="popular">
+          <div class="sub_title">
+            <span>MOST POPULAR ITEMS</span>
+          </div>
+
+          <div class='feature_item'>
+            <div class='first_item'>
+              <app-item></app-item>
+            </div>
+            <div>
+              <app-item></app-item>
+            </div>
+            <div>
+              <app-item></app-item>
+            </div>
+            <div class='right_item'>
+              <app-item></app-item>
+            </div>
+          </div>
+        </div>
+
+
+
 
         
     </div>
 </template>
 
 <script>
+  import Item from '../Item.vue';
+
     export default {
-        
+        components:{
+          appItem:Item
+        }
     }
 </script>
 
@@ -58,18 +117,22 @@
     
     
     @media screen and (min-width: 1300px) {
-        .images_change, .searchBar{
+        .images_change, .searchBar, .advertisements, .feature , .popular{
             width:1300px;
             margin: auto;
         }
 
-        .searchBar{
+        
+    }
+
+    .searchBar{
             background-color: black;
             display: flex;
             justify-content: center;
             padding:20px;
             margin-top: 20px;
         }
+        
         form div{
             margin-left: 30px;
         }
@@ -78,7 +141,52 @@
             font-size: 1.5em;
             color: white;
         }
-    }
+        form button{
+          margin-left: 30px;
+          font-size: 1.5em;
+          padding: 0 20px;
+        }
+
+        .advertisements{
+          margin-top: 20px;
+          display: flex;
+          justify-content: space-between;
+        }
+
+        .ad{
+          width:32%;
+          height: 350px;
+          background-color: gray;
+        }
+
+        .feature, .popular{
+          margin-top: 20px;
+        }
+        .first_item{
+          padding-left: 0;
+        }
+        .last_item{
+          padding-right: 0;
+        }
+        .feature_item{
+          display: flex;
+          
+        }
+        .feature_item div{
+          width:100%;
+        }
+
+        .sub_title{
+          padding: 10px 30px;
+          color: black;
+          background-color: yellow;
+          font-size: 1.5em;
+          font-weight: bold;
+        }
+
+        .popular{
+          margin-bottom: 30px;
+        }
     
       
 
