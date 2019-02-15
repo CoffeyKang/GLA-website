df30b8d849361a077ee42ae6883cb81de2af0af0
coffeykang <coffeykang@gmail.com>
2019-02-15 17:19:55 -0500

    add shipping to address in dealer po email

--- resources/views/emails/dealerSO.blade.php
+++ resources/views/emails/dealerSO.blade.php
@@ -21,7 +21,7 @@
             font-family: Arial, sans-serif;
             font-size: 14px;
             line-height: 24px;
-            
+
         }
 
         .body {
@@ -48,53 +48,70 @@
             font-size: 120%;
         }
 
-        .text-right{
+        .text-right {
             text-align: right;
         }
-        .text-left{
+
+        .text-left {
             text-align: left;
         }
-        hr{
+
+        hr {
             border-color: lightgray;
         }
-        .p265{
+
+        .p265 {
             width: 265px;
         }
-        .p60{
+
+        .p60 {
             width: 60px;
         }
-        .p530{width: 600px;}
-        .p300{
+
+        .p530 {
+            width: 600px;
+        }
+
+        .p300 {
             width: 300px;
         }
-        .p105{
+
+        .p105 {
             width: 105px;
 
         }
-        .p160{
+
+        .p160 {
             width: 160px;
         }
-        .pd-10{
-        	padding: 10px;
+
+        .pd-10 {
+            padding: 10px;
         }
-        .headerNav{
+
+        .headerNav {
             font-size: 20px;
             color: #1d4077;
             font-weight: 700;
-            border-bottom:1px solid black;
+            border-bottom: 1px solid black;
         }
-        .headerNav a{
+
+        .headerNav a {
             color: #1d4077;
         }
-        .headerNav a:hover{
+
+        .headerNav a:hover {
             text-decoration: none;
         }
-        a{ text-decoration: none; }
+
+        a {
+            text-decoration: none;
+        }
     </style>
 </head>
 
 <body>
-    
+
     <table class='body'>
         <tr>
             <td class='text-left'><img src="https://www.goldenleafautomotive.com/images/header_logo.png" alt="" width="80%;"></td>
@@ -104,10 +121,30 @@
         </tr>
         <tr class='content'>
             <td colspan=2 style='padding-top:30px'>
-            <span class='hi'>Dealer Account {{$dealerHistory->account}} / {{$dealer}} </span><br>
+                <span class='hi'>Dealer Account {{$dealerHistory->account}} / {{$dealer}} </span><br>
 
                 <b>The dealer order number # {{$dealerHistory->order_num}}</b><br>
-                <b>Notes # {{$dealerHistory->notes}}</b><br>
+                <b>Notes # {{$dealerHistory->notes}}</b><br> @if ($dealerHistory->dealerShipAddress===null) @else
+                <table width="600px" class='pd-10'>
+                    <tr>
+                        <td style='text-align: left;  '>
+                            <b><br>SHIPPING TO</b>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style='text-align: left; text-transform:uppercase; font-weight:700'>
+                            {{$dealerHistory->dealerShipAddress->company}}<br> {{$dealerHistory->dealerShipAddress->address}}
+                            <br> {{$dealerHistory->dealerShipAddress->city}} {{$dealerHistory->dealerShipAddress->province}}
+                            {{$dealerHistory->dealerShipAddress->postalcode}} @if($dealerHistory->dealerShipAddress->country=="CA")CANADA
+                            @else USA @endif
+                            <br>
+
+                        </td>
+
+
+                    </tr>
+                </table>
+                <br> @endif
 
                 <table style='font-size:10px;' width="600px;" class='p530'>
                     <tr>
@@ -117,27 +154,27 @@
                     </tr>
                     <tr style='border-bottom:1px solid gray;padding-top:30px'>
                         <th class='p60'>Item</th>
-                        <th  style='word-wrap: break-word; width:"290px"'>Description</th>
+                        <th style='word-wrap: break-word; width:"290px"'>Description</th>
                         <th class='p60'>Unit Price</th>
                         <th class='p60'>QTY</th>
                         <th class='p60 text-right'>Price</th>
                     </tr>
                     @foreach ($dealerHistory->dealerDetails as $item)
-                        <tr style='border-bottom:1px solid lightgray'>
-                            <td  class='p60'>{{$item->item}}</td>
-                            <td style='word-wrap: break-word; width:"290px"'>{{$item->itemInfo->descrip}}</td>
-                            <td class='p60' @if($item->sale==1) style='color:red' @endif>${{number_format($item->price,2)}}</td>
-                            <td class='p60'>{{$item->qty}}</td>                            
-                            <td class='p60 text-right' >${{number_format($item->price * $item->qty,2)}}</td>
-                        </tr>
+                    <tr style='border-bottom:1px solid lightgray'>
+                        <td class='p60'>{{$item->item}}</td>
+                        <td style='word-wrap: break-word; width:"290px"'>{{$item->itemInfo->descrip}}</td>
+                        <td class='p60' @if($item->sale==1) style='color:red' @endif>${{number_format($item->price,2)}}</td>
+                        <td class='p60'>{{$item->qty}}</td>
+                        <td class='p60 text-right'>${{number_format($item->price * $item->qty,2)}}</td>
+                    </tr>
                     @endforeach
                 </table>
                 <br>
                 <table style='font-size:12px;' width="600px;" class='p530 text-center'>
-                
+
                     <tr>
                         <td class='p300 text-right'>
-                
+
                         </td>
                         <td>
                             <table width="600px;" class='p300 text-right'>
@@ -156,15 +193,15 @@
                             </table>
                         </td>
                     </tr>
-                
+
                 </table>
-            </span>
+                </span>
             </td>
         </tr>
         <br>
         <tr>
-            <td class='footer' colspan=2>170 Zenway Blvd. Unit 2, Woodbridge, ON., L4H 2Y7 CANADA | 905-850-3433<br> &copy; {{date("Y")}} Golden Leaf Automotive.
-                All rights reserved.</td>
+            <td class='footer' colspan=2>170 Zenway Blvd. Unit 2, Woodbridge, ON., L4H 2Y7 CANADA | 905-850-3433<br> &copy; {{date("Y")}} Golden Leaf
+                Automotive. All rights reserved.</td>
         </tr>
     </table>
 
